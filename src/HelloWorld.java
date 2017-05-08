2346f9c91d4c1aabe75872b51f37864fc53ffa8a
Amberlee <amberleec12@yahoo.com>
2017-05-08 13:35:39 -0500

    Did the first exercise to show what i can and cannot do with Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,52 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "My favorite number is 7!";
+//        System.out.println(myString);
+//
+//        myString = "H";
+//        System.out.println(myString);
+//    // cant float if myString is already defined
+//        float myString = 3.14159;
+//        System.out.println(myString);
+//        // wrong way, need to always assign
+//        long myNumber;
+//        System.out.println(myNumber);
+//    // again cant float if myNumber is already defined
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        // again cant float if myNumber is already defined
+//        float myNumber = 123L;
+//        System.out.println(myNumber);
+//        // again cant float if myNumber is already defined
+//        float myNumber = 123;
+//        System.out.println(myNumber);
+//        // again cant float if myNumber is already defined
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        int x = 4;
+//        System.out.println(x+=5);
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y*=x);
+
+        int x = 10;
+        int y = 2;
+        System.out.println(x/=y);
+        System.out.println(y-=x);
+
     }
 }
