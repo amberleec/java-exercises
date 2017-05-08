6122ede7ef86088b30a436d8e6d48461556145ad
Amberlee <amberleec12@yahoo.com>
2017-05-08 15:24:29 -0500

    Working on the exercise trying to learn how to properly use the int and string

---
+++ src/io/playWithIO.java
@@ -0,0 +1,15 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * Created by Amberlee on 5/8/17.
+ */
+public class playWithIO {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello");
+    }
+}
