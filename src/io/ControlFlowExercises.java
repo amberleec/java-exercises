555fae377df3c2bdd1c21e79eff743efb539c3ce
Amberlee <amberleec12@yahoo.com>
2017-05-10 15:50:10 -0500

    Table/chart

--- src/io/ControlFlowExercises.java
+++ src/io/ControlFlowExercises.java
@@ -8,7 +8,7 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         int first = 5;
 
-        while(first<=15) {
+        while (first <= 15) {
             System.out.println("First number" + first);
             first++;
         }
@@ -16,36 +16,37 @@ public class ControlFlowExercises {
         int second = 0;
 
         do {
-            second+=2;
+            second += 2;
             System.out.println("Second number" + second);
 
-        } while (second<=100);
+        } while (second <= 100);
 
         int negative = -10;
 
         do {
-            negative+=5;
+            negative += 5;
             System.out.println("Negative number" + negative);
 
         } while (negative <= 100);
-//
+
+
         long begining = 2;
 
         do {
-            begining = begining*begining;
+            begining = begining * begining;
             System.out.println("Begining number" + begining);
 
-        } while(begining<=1000000);
+        } while (begining <= 1000000);
 
-        for(int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (((i % 3) == 0) && ((i % 5) == 0))
                 System.out.print("fizzbuzz");
-            else if ((i % 3) == 0) System.out.print("fizz");
-            else if ((i % 5) == 0) System.out.print("buzz");
+            else if ((i % 3) == 0)
+                System.out.print("fizz");
+            else if ((i % 5) == 0)
+                System.out.print("buzz");
             else System.out.print(i);
             System.out.print(" ");
+        }
     }
 }
-
-
-    }
---
+++ src/io/TableOfPowers.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * Created by Amberlee on 5/10/17.
+ */
+public class TableOfPowers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What number would you like to go up to?");
+        int limit = input.nextInt();
+
+        System.out.println("Here is your table!");
+
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int i = 1; i <= limit; i++) {
+            System.out.printf("%d      | %d       | %d%n", i, i*i, i*i*i);
+        }
+    }
+}
