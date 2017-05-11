4be7eb04b41b67fdb9f153c075e73a125fe52c66
Amberlee <amberleec12@yahoo.com>
2017-05-11 11:43:00 -0500

    Bobs exercise. XTRA exercise

---
+++ src/io/BobExercise.java
@@ -0,0 +1,24 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * Created by Amberlee on 5/11/17.
+ */
+public class BobExercise {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Talk with Bob:");
+        String message = input.nextLine();
+
+        if(message.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (message.isEmpty()) {
+            System.out.println("Fine. be that way!");
+        } else if (message.endsWith("!") || message.toUpperCase().equals(message)) {
+            System.out.println("Whoa, chill out!");
+        }
+
+    }
+}
