f6e141c9d61e3a805be5f9aedc214f67b63ba88c
Amberlee <amberleec12@yahoo.com>
2017-05-12 10:21:28 -0500

    but the return in

--- src/io/Methods.java
+++ src/io/Methods.java
@@ -3,7 +3,7 @@ package io;
 /**
  * Created by Amberlee on 5/12/17.
  */
-public class Methods  {
+public class Methods {
     public static void main(String[] args) {
 
         addition(2);
@@ -11,53 +11,54 @@ public class Methods  {
         multiplication(5);
         division(8);
         modulus(2);
-        multipy(5,5);
+        multipy(5, 5);
     }
 
-    public static void addition (int number) {
+    public static void addition(int number) {
         System.out.println("Put in a number and add it!");
         int num = number;
-        System.out.println(num+num);
+        System.out.println(num + num);
     }
 
-    public static void subtraction (int number) {
+    public static void subtraction(int number) {
         System.out.println("Put in a number and subtraction!");
         int num = number;
-        System.out.println(num-6);
+        System.out.println(num - 6);
     }
 
-    public static void multiplication (int number) {
+    public static void multiplication(int number) {
         System.out.println("Put in a number and multiply!");
         int num = number;
-        System.out.println(num*num);
+        System.out.println(num * num);
     }
 
-    public static void division (int number) {
+    public static void division(int number) {
         System.out.println("Put in a number and divide it!");
         int num = number;
-        System.out.println(num/2);
+        System.out.println(num / 2);
     }
 
-    public static void modulus (int number) {
+    public static void modulus(int number) {
         System.out.println("Try out Modulus function!");
         int num = number;
-        System.out.println(20%num);
-        System.out.println(6%num);
+        System.out.println(20 % num);
+        System.out.println(6 % num);
     }
 
-    public static void multipy (int number, int x) {
+    public static void multipy(int number, int x) {
         int num = number;
         int s = x;
-        for(int i = 0; i < s; i++){
-            num+=s;
+        for (int i = 0; i < s; i++) {
+            num += s;
         }
         System.out.println(num++);
     }
 
-    public static int getInteger(int min, int max){
+    public static int getInteger(int min, int max) {
         System.out.print("Enter a number between 1 and 10: ");
         int userInput = getInteger(1, 10);
         System.out.println("Try again if it does'nt work out!" + userInput);
+        return userInput;
     }
 
 
