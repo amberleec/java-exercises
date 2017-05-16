bc77f2d1b200b22f16b8589e5991f11da7e9cbbc
Amberlee <amberleec12@yahoo.com>
2017-05-16 15:45:52 -0500

    Trying to get the people down

--- src/Java2/Person.java
+++ src/Java2/Person.java
@@ -4,4 +4,19 @@ package Java2;
  * Created by Amberlee on 5/16/17.
  */
 public class Person {
+
+    public String getName();
+    public void setName(String name);
+
+
+    Person person1 = new Person("Ethan");{
+        System.out.println(person1.getName().equals(person2.getName()));
+    }
+
+
+    Person person2 = new Person("Cheyenne");{
+        System.out.println(person1 == person2);
+    }
+
+
 }
