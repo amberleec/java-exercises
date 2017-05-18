7e98a7556d63ff9201fa50ae4902a8386276d6c6
Amberlee <amberleec12@yahoo.com>
2017-05-18 16:46:48 -0500

    Movies array

--- src/Java2/ArraysExercise.java
+++ src/Java2/ArraysExercise.java
@@ -6,7 +6,14 @@ package Java2;
 public class ArraysExercise {
     public static void main(String[] args) {
 
-        String
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view movies in the animated category");
+        System.out.println("3 - view movies in the drama category");
+        System.out.println("4 - view movies in the horror category");
+        System.out.println("5 - view movies in scifi category");
+    }
+
 
         public class MoviesArray {
             public static Movie[] findAll() {
