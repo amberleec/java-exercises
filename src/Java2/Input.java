d98de2adacd7b8ac81caa8578b9f06aa8a33f624
Amberlee <amberleec12@yahoo.com>
2017-05-25 08:49:20 -0500

    input exercise

---
+++ src/Java2/Input.java
@@ -0,0 +1,61 @@
+package Java2;
+
+import java.util.Scanner;
+/**
+ * Created by Amberlee on 5/25/17.
+ */
+public class Input {
+    private Scanner input;
+
+    public Input(Scanner userInput){
+        this.input = new Scanner(System.in);
+    }
+
+    public Input(){
+        this.input = new Scanner(System.in);
+    }
+
+    public String getString(){
+        return input.nextLine();
+    }
+
+    public boolean yesNo(){
+        String answer = this.getString();
+        if (answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")){
+            return true;
+        }
+        return false;
+    }
+
+    public int getInt(int min, int max) {
+        int answer = this.getInt();
+        if (answer < min || answer > max){
+            System.out.println("Number out of range, try again");
+            getInt(min, max);
+            input.nextInt();
+            return answer;
+        }
+        System.out.println("Correct");
+        return answer;
+    }
+
+    public int getInt(){
+        return input.nextInt();
+    }
+
+    public double getDouble(double min, double max){
+        double answer = this.getDouble();
+        if (answer < min || answer > max){
+            System.out.println("Number out of range, try again");
+            getDouble(min, max);
+            input.nextDouble();
+            return answer;
+        }
+        System.out.println("Correct");
+        return answer;
+    }
+
+    public double getDouble(){
+        return input.nextDouble();
+    }
+}
